03c01d6bc66310d518182e18d120a837a1bead49
patrickpollet <pp@patrickpollet.net>
2010-03-07 11:55:40 +0000

    patches 3m mars 2010
    git-svn-id: file:///home/svn2/moodle/moodle.195/wspp@365 47220bad-2fbe-4483-8024-c4516a9bd1b0

--- clients/MoodleWS.php
+++ clients/MoodleWS.php
@@ -340,7 +340,7 @@ class MoodleWS {
 
   public $client;
 
-  private $uri = 'http://prope.insa-lyon.fr/moodle.195/wspp/wsdl';
+  private $uri = 'http://cipcnet.insa-lyon.fr/moodle.195/wspp/wsdl';
 
   public function MoodleWS($wsdl = "http://localhost/moodle.195/wspp/wsdl_pp.php", $uri=null, $options = array()) {
     if($uri != null) {
