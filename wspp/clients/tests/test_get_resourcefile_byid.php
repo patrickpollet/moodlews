6055e86004b312941e6cf6636ddeb785bc8f0112
patrickpollet <pp@patrickpollet.net>
2011-04-13 15:09:58 +0200

    strong improvments of get_grades operations. A course idnumber not anymore required

---
+++ wspp/clients/tests/test_get_resourcefile_byid.php
@@ -0,0 +1,24 @@
+<?php
+require_once ('../classes/MoodleWS.php');
+
+$client=new MoodleWS();
+require_once ('../auth.php');
+/**test code for MoodleWS: Get a resource file by it instance id
+* @param int $client
+* @param string $sesskey
+* @param int $resourceid
+* @return  fileRecord
+*/
+
+$lr=$client->login(LOGIN,PASSWORD);
+$res=$client->get_resourcefile_byid($lr->getClient(),$lr->getSessionKey(),195);
+print_r($res);
+print($res->getFileurl());
+print($res->getFilename());
+print($res->getFilepath());
+print($res->getFilesize());
+print($res->getFilecontent());
+
+$client->logout($lr->getClient(),$lr->getSessionKey());
+
+?>
