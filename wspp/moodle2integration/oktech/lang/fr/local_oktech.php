e65672b95b2aec443957f95c6b15f7700a2ff79a
patrickpollet <pp@patrickpollet.net>
2012-01-24 12:48:09 +0100

    added 'pluginname' string entry in language files for Moodle 2.x also for Moodle 2.x integration

---
+++ wspp/moodle2integration/oktech/lang/fr/local_oktech.php
@@ -0,0 +1,6 @@
+<?PHP // $Id$
+      // local_oktech.php -
+      // translation file for Ok Tech WebServices (wspp) version 1.6
+
+$string['pluginname']='OK Tech Web Services Moodle 2.x integration';
+?>
