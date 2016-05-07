fe6b9c7444ee889d47e3f89c948295c3fbdc4c53
Logan Reynolds <logan.reynolds@remote-learner.net>
2016-05-06 18:19:28 -0600

    MAE-2207 - Added component value for M30

--- wspp/local/wspp/version.php
+++ wspp/local/wspp/version.php
@@ -1,6 +1,5 @@
 <?php
 
 $plugin->version  = 2010101000;   // The (date) version of this plugin
-//$plugin->requires = 2010021900;   // Requires this Moodle version
-
-?>
+$plugin->requires = 2010112400;   // Requires this Moodle version
+$plugin->component = 'local_wspp';
